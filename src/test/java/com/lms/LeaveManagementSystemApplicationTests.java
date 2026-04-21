6c3f00a4c15963db6e778f4b9a817df7ed60d877
kahkei <kahkei777@gmail.com>
2026-04-21 12:23:22 +0800

    Add build and test automation

--- src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
+++ src/test/java/com/lms/LeaveManagementSystemApplicationTests.java
@@ -1,13 +1,23 @@
 package com.lms;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-
-/*@RunWith(SpringRunner.class)
-@SpringBootTest*/
 public class LeaveManagementSystemApplicationTests {
 
-	//@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
 
+    @Test
+    public void testStringNotNull() {
+        String str = "Hello";
+        assertNotNull(str);
+    }
+
+    @Test
+    public void testBooleanTrue() {
+        assertTrue(5 > 3);
+    }
 }
